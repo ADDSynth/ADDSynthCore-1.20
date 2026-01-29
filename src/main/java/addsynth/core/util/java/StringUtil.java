9e54bc808b3470721fd6ec99533f276f6e2d1a03
ADDSynth <addsynth@gmail.com>
2026-01-29 13:58:21 -0600

    Added addNewLineComponent() to StringUtil

--- src/main/java/addsynth/core/util/java/StringUtil.java
+++ src/main/java/addsynth/core/util/java/StringUtil.java
@@ -2,12 +2,15 @@ package addsynth.core.util.java;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import addsynth.core.ADDSynthCore;
 import addsynth.core.util.color.Color;
 import addsynth.core.util.math.common.CommonMath;
 import addsynth.core.util.math.common.RoundMode;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.Vec3i;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
@@ -40,6 +43,37 @@ public final class StringUtil {
       b.append(s);
     }
     return b.toString();
+  }
+
+  /** <p>The Minecraft engine will usually handle new line characters '{@code \n}' in
+   *  {@link Component Components}, but one place where it isn't handled correctly is in
+   *  ItemStack Tooltips. Newline characters are handled inconsistantly. In cases such
+   *  as this, you can use this function. This will split the text Component at the new
+   *  line points into separate Components and add them to the List of Components.
+   *  <p>Do not use style codes in the text strings as that is generally not supported
+   *  anymore, but if you do, be aware that the new Components will not have the same style
+   *  as the original, so you'll need to reapply style codes after each new line character.
+   * @param components
+   * @param text_component
+   */
+  public static final void addNewLineComponent(final List<Component> components, final Component text_component){
+    String text = text_component.getString();
+    int position = text.indexOf('\n');
+    if(position == -1){
+      components.add(text_component);
+      return;
+    }
+    final Style style = text_component.getStyle();
+    String first_string;
+    do{
+      first_string = text.substring(0, position);
+      text = text.substring(position+1);
+      components.add(Component.literal(first_string).withStyle(style));
+      position = text.indexOf('\n');
+    }
+    while(position >= 0);
+    // add last string
+    components.add(Component.literal(text).withStyle(style));
   }
 
   public static final String toPercentageString(final double value){
