019318e138155a8a43cb5508826726bedc426185
ADDSynth <addsynth@gmail.com>
2026-02-04 22:19:32 -0600

    Update ArrayUtil

--- src/main/java/addsynth/core/util/java/ArrayUtil.java
+++ src/main/java/addsynth/core/util/java/ArrayUtil.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.function.Function;
+import java.util.Objects;
+import java.util.function.IntFunction;
 import javax.annotation.Nonnull;
 import addsynth.core.ADDSynthCore;
 
@@ -18,129 +19,37 @@ public final class ArrayUtil {
     return index >= 0 && index < array.length;
   }
 
-  /** This will attempt to return the value at index in the array.
-   *  Throws an error if array is null or of length 0.
-   *  Returns value at index 0 if index is out of bounds.
-   * @param array
-   * @param index
-   * @deprecated because this doesn't ask for a default value in case of an error, and throws exceptions, crashing the game.
-   */
-  @Deprecated
-  public static final <T> T getArrayValue(final T[] array, final int index){
-    return getArrayValue(array, index, false);
-  }
-
-  /** This will attempt to return the value at index in the array.
-   *  Throws an error if array is null or of length 0.
-   *  Returns value at index 0 if index is out of bounds.
-   *  Logs a warning that index was out of bounds if warn is set to true.
-   * @param array
-   * @param index
-   * @param warn
-   * @deprecated because this doesn't ask for a default value in case of an error, and throws exceptions, crashing the game.
-   */
-  @Deprecated
-  public static final <T> T getArrayValue(final T[] array, final int index, final boolean warn){
-    if(array == null){
-      throw new NullPointerException("Input array for "+ArrayUtil.class.getName()+".getArrayValue() is null!");
-    }
-    final int length = array.length;
-    if(length == 0){
-      throw new IllegalArgumentException("Input array for "+ArrayUtil.class.getName()+".getArrayValue() doesn't have any values!");
-    }
-    if(isInsideBounds(index, length) == false){
-      final T default_value = array[0];
-      if(warn){
-        print_array_index_out_of_bounds_error(array, index);
-        ADDSynthCore.log.warn("Returning the assumed default value in index 0 instead!");
-      }
-      return default_value;
-    }
-    return array[index];
-  }
-
-  /**
-   * Returns the value at index in the array. Returns the default value if
-   * any error occured.
-   * @param array
-   * @param index
-   * @param default_value
-   */
-  public static final <T> T getArrayValue(final T[] array, final int index, final T default_value){
-    return getArrayValue(array, index, default_value, false);
-  }
-
-  /**
-   * Returns the value at index in the array. Returns the default value if
-   * any error occured.
-   * Logs a warning that index was out of bounds if warn is true.
-   * @param array
-   * @param index
-   * @param default_value
-   * @param warn
-   */
-  public static final <T> T getArrayValue(final T[] array, final int index, final T default_value, final boolean warn){
-    if(array == null){
-      if(warn){
-        ADDSynthCore.log.error("Input array for "+ArrayUtil.class.getName()+".getArrayValue() is null!", new NullPointerException());
-      }
-      return default_value;
-    }
-    final int length = array.length;
-    if(length == 0){
-      if(warn){
-        ADDSynthCore.log.error("Input array for "+ArrayUtil.class.getName()+".getArrayValue() doesn't have any values!", new IllegalArgumentException());
-      }
-      return default_value;
-    }
-    if(isInsideBounds(index, length) == false){
-      if(warn){
-        print_array_index_out_of_bounds_error(array, index);
-      }
-      return default_value;
-    }
-    return array[index];
-  }
-
-  /** Returns false if array is null or empty. */
-  public static final boolean is_valid_array(final Object[] array){
-    if(array == null){
-      ADDSynthCore.log.error("Input array is null.");
-      return false;
-    }
-    if(array.length == 0){
-      ADDSynthCore.log.error("Input array is empty.");
-      return false;
-    }
-    return true;
-  }
-
-  // TEST can I just pass an object array?
-  public static final <T> boolean is_valid_array(final T[] array, final String call_location){
-    if(array == null){
-      ADDSynthCore.log.error("Input for "+call_location+" was null!", new NullPointerException());
-      return false;
-    }
-    if(array.length == 0){
-      ADDSynthCore.log.error("Input array for "+call_location+" requires at least 1 "+array.getClass().getComponentType().getSimpleName()+" element.", new IllegalArgumentException());
-      return false;
-    }
-    return true;
-  }
-  
-  /** Checks array index and prints an ArrayIndexOutofBounds Error if necessary. */
-  public static final <T> boolean check_array_index(final T[] array, final int index){
-    if(is_valid_array(array)){
-      if(index >= 0 && index < array.length){
+  /** Used to check if the array contains the value. */
+  public static final <T> boolean valueExists(final T[] array, final T value){
+    for(final T i : array){
+      if(Objects.equals(i, value)){
         return true;
       }
-      print_array_index_out_of_bounds_error(array, index);
     }
     return false;
   }
 
+  /**
+   * Returns the value at index in the array. Returns the default value if any error occured.
+   * @param array
+   * @param index
+   * @param default_value
+   */
+  // Energy Suspension Bridge in Overpowered Technology uses this, when loading enum index values
+  public static final <T> T getArrayValue(final T[] array, final int index, final T default_value){
+    if(array == null){
+      ADDSynthCore.log.error("Input array for "+ArrayUtil.class.getName()+".getArrayValue() is null!", new NullPointerException());
+      return default_value;
+    }
+    if(isInsideBounds(index, array.length)){
+      return array[index];
+    }
+    print_array_index_out_of_bounds_error(array, index);
+    return default_value;
+  }
+
   /** Prints extremely detailed information to the log, and also prints the stacktrace. */
-  public static final <T> void print_array_index_out_of_bounds_error(@Nonnull final T[] array, final int index){
+  public static final void print_array_index_out_of_bounds_error(@Nonnull final Object[] array, final int index){
     final int length = array.length;
     final StringBuilder s = new StringBuilder();
     s.append("Invalid index ");
@@ -150,7 +59,19 @@ public final class ArrayUtil {
     s.append('[');
     s.append(length);
     s.append("]. ");
-    s.append(length == 0 ? "The array is empty." : length == 1 ? "Only index 0 is valid." : "Only indexes 0-"+(length-1)+" are valid.");
+    if(length == 0){
+      s.append("The array is empty.");
+    }
+    else{
+      if(length == 1){
+        s.append("Only index 0 is valid.");
+      }
+      else{
+        s.append("Only indexes 0 to ");
+        s.append(length - 1);
+        s.append(" are valid.");
+      }
+    }
     ADDSynthCore.log.error(s.toString(), new ArrayIndexOutOfBoundsException());
   }
 
@@ -163,18 +84,6 @@ public final class ArrayUtil {
       }
     }
     return total_length;
-  }
-
-  /** Used to check if the array contains the value. */
-  public static final <T> boolean valueExists(final T[] array, final T value){
-    boolean exists = false;
-    for(final T i : array){
-      if(i.equals(value)){
-        exists = true;
-        break;
-      }
-    }
-    return exists;
   }
 
   public static final long[] combine_arrays(@Nonnull final long[] a, @Nonnull final long[] b){
@@ -211,7 +120,7 @@ public final class ArrayUtil {
   }
 
   @SafeVarargs
-  public static final <T> T[] combine_collections(@Nonnull final Function<Integer, T[]> new_array_supplier, final Collection<T> ... lists){
+  public static final <T> T[] combine_collections(@Nonnull final IntFunction<T[]> new_array_supplier, final Collection<T> ... lists){
     // check for null
     if(lists == null){
       return new_array_supplier.apply(0);
@@ -238,84 +147,45 @@ public final class ArrayUtil {
   }
 
   /** This checks the two arrays and returns true if they are different.
-   *  Only replaces the cached_array if the size changed. */
+   *  @deprecated Use !Arrays.equals(Object[], Object[]) instead. */
+  // REMOVE in 2031
+  @Deprecated
   public static final <T> boolean arrayChanged(final T[] cached_array, final T[] new_array){
-    if(new_array.length != cached_array.length){
-      return true;
-    }
-    boolean changed = false;
-    int i;
-    final int length = new_array.length;
-    for(i = 0; i < length; i++){
-      if(cached_array[i].equals(new_array[i]) == false){
-        changed = true;
-        break;
-      }
-    }
-    return changed;
+    return !Arrays.equals(cached_array, new_array);
   }
 
   /** This checks the array against the supplied {@link ArrayList} to see if the array needs to be updated. */
+  @Deprecated
   public static final <T> boolean arrayChanged(final T[] cached_array, final ArrayList<T> list){
-    if(list.size() != cached_array.length){
+    final int length = cached_array.length;
+    if(list.size() != length){
       return true;
     }
-    boolean changed = false;
-    int i;
-    final int length = cached_array.length;
-    for(i = 0; i < length; i++){
-      if(list.get(i).equals(cached_array[i]) == false){
-        changed = true;
-        break;
+    for(int i = 0; i < length; i++){
+      if(!Objects.equals(cached_array[i], list.get(i))){
+        return true;
       }
     }
-    return changed;
+    return false;
   }
 
   /** This checks the data in the new_array against the data in the cached array and updates the
-   *  cached_array accordingly. Returns true if an update occured. */
-  public static final <T> boolean updateArray(T[] cached_array, final T[] new_array){
-    boolean changed = false;
-    final int length = new_array.length;
-    if(cached_array.length == length){
-      int i;
-      for(i = 0; i < length; i++){
-        if(cached_array[i].equals(new_array[i]) == false){
-          changed = true;
-          cached_array[i] = new_array[i];
-        }
-      }
-    }
-    else{
-      cached_array = new_array;
-      changed = true;
-    }
-    return changed;
+   *  cached_array accordingly. Returns true if an update occured.
+   *  @deprecated Use Arrays.equals(a[], b[]) ? a : b instead. */
+  // REMOVE in 2031
+  @Deprecated
+  public static final <T> T[] updateArray(final T[] cached_array, final T[] new_array){
+    return Arrays.equals(cached_array, new_array) ? cached_array : new_array;
   }
 
   /** This checks the cached_array against the data stored in the supplied list, and updates
    *  it if there were any changes. Returns true if an update occured.
-   *  @deprecated check to see if this works first.
    */
-  public static final <T> boolean updateArray(T[] cached_array, final ArrayList<T> list){
-    boolean changed = false;
-    final int length = list.size();
-    if(cached_array.length == length){
-      int i;
-      T e;
-      for(i = 0; i < length; i++){
-        e = list.get(i);
-        if(e.equals(cached_array[i]) == false){
-          changed = true;
-          cached_array[i] = e;
-        }
-      }
-    }
-    else{
-      cached_array = (T[])list.toArray(); // list.toArray(new T[length]);
-      return true;
-    }
-    return changed;
+  // REMOVE in 2031
+  @Deprecated
+  @SuppressWarnings("unchecked")
+  public static final <T> T[] updateArray(final T[] cached_array, final ArrayList<T> list){
+    return updateArray(cached_array, (T[])list.toArray());
   }
 
   /** Syncs the data in the cached_list with that in the supplied list. Items are
@@ -332,30 +202,24 @@ public final class ArrayUtil {
     return changed;
   }
 
-  /** Syncs the data in the cached_list with that in the supplied list. First the cached
-   *  list is resized to match the new list, then each Item is copied over directly. This
-   *  ensures the order matches that of the new list. Returns true if an update occured. */
+  /** Syncs the data in the cached_list with that in the supplied list. The cached list is
+   *  resized, and each element is copied over one by one. This ensures the order matches
+   *  that of the new list. Returns true if an update occured. */
   public static final <T> boolean syncListExactly(final List<T> cached_list, final List<T> new_list){
-    boolean changed = false;
     final int length = new_list.size();
-    while(cached_list.size() > length){
-      cached_list.remove(length);
-      changed = true;
+    if(cached_list.size() != length){
+      cached_list.clear();
+      cached_list.addAll(new_list);
+      return true;
     }
-    final int old_length = cached_list.size();
-    int i;
+    boolean changed = false;
     T a;
-    for(i = 0; i < length; i++){
+    for(int i = 0; i < length; i++){
       a = new_list.get(i);
-      if(i < old_length){
-        if(cached_list.get(i).equals(a) == false){
-          cached_list.set(i, a);
-          changed = true;
-        }
-        continue;
+      if(!Objects.equals(cached_list.get(i), a)){
+        cached_list.set(i, a);
+        changed = true;
       }
-      cached_list.add(a);
-      changed = true;
     }
     return changed;
   }
